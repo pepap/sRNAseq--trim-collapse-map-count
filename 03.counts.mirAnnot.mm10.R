1178f7fe4780b5c6065feaae0d1ab3d9826f7053
pepap <jpasulka@gmail.com>
2026-02-11 16:04:49 +0100

    Update 03.counts.mirAnnot.mm10.R

--- 03.counts.mirAnnot.mm10.R
+++ 03.counts.mirAnnot.mm10.R
@@ -1,47 +1,34 @@
-source("~/Scripts/R_func/collapsedBam2counts.R")
+source("03.01.collapsedBam2counts.R")
 
-#load( "/storage/brno1-cerit/home/pepap/DicerX/00.stats/02.DE/mirAnnot.dt.rda",verbose=T )
-load("/storage/brno12-cerit/home/pepap/brno1/DicerX/11.check_miRNAstrand/mirAnnot.pepType.dt.rda",verbose=T)
+# @pepap : load the mature miRNA annotation
+load("mirAnnot.pepType.dt.rda",verbose=T)
 mirAnnot.dt <- mirAnnot.pepType.dt
 
+# @pepap : SUFFIX of the input BAM files
 SUFF  <- ".se.Aligned.sortedByCoord.out.bam"
 
-#ESTOR <- "/storage/brno12-cerit/home/pepap/brno1/Valeria.Buccheri/02.mESC_mutants/BAM/"
-#EBAMS <- system( command=paste0("/bin/ls ",ESTOR,"*/*",SUFF),intern=T )
-#ECONS <- gsub(   SUFF,"",gsub( "^.*[/]","",EBAMS ) )
-#
-#FSTOR <- "/storage/brno12-cerit/home/pepap/brno1/Valeria.Buccheri/15.20231212_Buccheri/BAM/"
-#FBAMS <- system( command=paste0("/bin/ls ",FSTOR,"*/*",SUFF),intern=T )
-#FCONS <- gsub(   SUFF,"",gsub( "^.*[/]","",FBAMS ) )
-#
-#LSTOR <- "/storage/brno12-cerit/home/pepap/brno1/Valeria.Buccheri/16.mESC-DicerLoops/BAM/"
-#LBAMS <- system( command=paste0("/bin/ls ",LSTOR,"*/*",SUFF),intern=T )
-#LCONS <- gsub(   SUFF,"",gsub( "^.*[/]","",LBAMS ) )
-#
-#MSTOR <- "/storage/brno12-cerit/home/pepap/brno1/Valeria.Buccheri/17.240924_mESC_loops_mutants/BAM/"
-#MBAMS <- system( command=paste0("/bin/ls ",MSTOR,"*/*",SUFF),intern=T )
-#MCONS <- gsub(   SUFF,"",gsub( "^.*[/]","",MBAMS ) )
+SSTOR <- "/path/to/mESC--Dicer-loop-mutants--20250411/BAM/"
+SBAMS <- system( command=paste0("/bin/ls ",SSTOR,"*/*",SUFF),intern=T )
+SCONS <- gsub(   SUFF,"",gsub( "^.*[/]","",SBAMS ) )
 
-CSTOR <- "/storage/brno12-cerit/home/pepap/brno1/Valeria.Buccheri/22.mESC--Dicer-loop-mutants--TraPR--20250516/BAM/"
-CBAMS <- system( command=paste0("/bin/ls ",CSTOR,"*/*",SUFF),intern=T )
-CCONS <- gsub(   SUFF,"",gsub( "^.*[/]","",CBAMS ) )
+TSTOR <- "/path/to/mESC--Dicer-loop-mutants--TraPR--20250516/BAM/"
+TBAMS <- system( command=paste0("/bin/ls ",TSTOR,"*/*",SUFF),intern=T )
+TCONS <- gsub(   SUFF,"",gsub( "^.*[/]","",TBAMS ) )
 
-#aBAMS <- c( EBAMS,FBAMS,LBAMS,MBAMS )
-#aCONS <- c( ECONS,FCONS,LCONS,MCONS )
-aBAMS <- c(CBAMS)
-aCONS <- c(CCONS)
+aBAMS <- c( SBAMS,TCONS )
+aCONS <- c( SCONS,TCONS )
 
 MINOVRL=15
-#> remove spliced reads
+# @pepap : remove spliced reads
 RMV.SPL=as.logical("TRUE")
-#> remove soft-clipped reads
+# @pepap : remove soft-clipped reads
 RMV.SCL=as.logical("FALSE")
-#> read-length range
+# @pepap : read-length range
 RLENRAN=c(19,32)
-#> allowed number of edit distances
+# @pepap : allowed number of edit distances
 NMRANGE=c(0,100)
 
-#= FUN
+#  @pepap : FUN
 dt2gr <- function(inDT,dt.seqnames="chr",dt.start="start",dt.end="end",dt.strand="strand",keep.metaData=T) {
 
  out.gr <-
@@ -95,7 +82,6 @@ for ( i in seq_along(aBAMS) ) {
 
  frac.dt                            <- merge( frac.dt,tmp.cnt[["EXP"]],by="biotype",all.x=T,sort=F )
  colnames(frac.dt)[ ncol(frac.dt) ] <- aCONS[i]
-# frac.dt[[aCONS[i]]]             <- frac.dt[[aCONS[i]]]/(tmp.cnt$NORM*1e-06)
  libs.dt                            <- rbind( libs.dt,data.table( libname=aCONS[i],libsize=tmp.cnt$NORM ) )
 
  assign( x=paste0(aCONS[i],".frac.dt"),value=tmp.cnt )
@@ -110,4 +96,3 @@ for ( i in seq_along(aBAMS) ) {
 
 save( list=objs.ls,file="counts.mirAnnot.rda" )
 save( list=c("frac.dt","libs.dt","mirAnnot.dt"),file="all_objs.rda" )
-
